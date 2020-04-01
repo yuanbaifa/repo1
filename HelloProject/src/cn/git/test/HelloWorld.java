a387167bb25110010d7b7161dd1928a6cfacb18e
y83103220 <yuanguanghui@huawei.com>
2020-04-01 20:46:01 +0800

    添加HelloProject工程至git

---
+++ HelloProject/src/cn/git/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.git.test;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("he11111111");
+        System.out.println("he11111111");
+        System.out.println("he11111111");
+        System.out.println("he11111111");
+    }
+}
